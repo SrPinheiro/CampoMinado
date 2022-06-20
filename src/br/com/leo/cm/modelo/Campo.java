3e3e01fecceef3de872eb95bc4c0805a8576fd8b
SrPinheiro <sr.pinheiro.ifnmg@gmail.com>
2022-06-20 02:12:50 -0300

    CampoMinado01

--- src/br/com/leo/cm/modelo/Campo.java
+++ src/br/com/leo/cm/modelo/Campo.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Campo {
+
     private final int linha;
     private final int coluna;
 
     private boolean aberto = false;
     private boolean minado = false;
     private boolean marcado = false;
-
-    
     private List<Campo> vizinhos = new ArrayList<>();
 
     public Campo(int linha, int coluna) {
