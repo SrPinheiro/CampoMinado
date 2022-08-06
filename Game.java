1fff51865cbcafa1b52741c9ab5f6da268214b8c
Leonardo Pinheiro <Sr.Pinheiro.IFNMG@gmail.com>
2022-08-05 22:49:16 -0300

    CampoMinado 2.0

---
+++ Game.java
@@ -0,0 +1,16 @@
+import br.com.leo.cm.visao.TelaPrincipal;
+
+public class Game {
+    public static void main(String[] args) {
+        new TelaPrincipal("IFNMG - CampoMinado LeonardoPinheiro");
+    }
+}
+
+/*
+ * Codigo feito por Leonardo Pinheiro
+ * IDE: Intellij IDEA — JetBrains
+ * Turma: Info 0121
+ * IFNMG — Campus Almenara
+ * GitHub: https://github.com/SrPinheiro
+ * Data: 05/08/2022
+ */
